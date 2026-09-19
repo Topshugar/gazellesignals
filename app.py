10e4f98015b9eb14a2dafcdb1363d3ec4978ae4d
Topshugar <80433952+Topshugar@users.noreply.github.com>
2026-09-19 15:29:49 +0100

    Load environment variables from .env file

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
